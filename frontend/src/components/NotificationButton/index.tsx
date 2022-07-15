91890056078084a42f31b02977a6ad9b70d37470
shinoshinohara <vshinohara@outlook.com>
2022-07-15 07:42:51 -0400

    send notification based on id

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,11 +1,24 @@
+import axios from 'axios';
 import icon from '../../assets/vector.svg'
+import { BASE_URL } from '../../utils/request';
 import './style.css'
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+
+
+function handleClick(saleId: number) {
+    axios(`${BASE_URL}/sales/${saleId}/notification`).then(response => {
+        console.log("SUCESSO");
+    })
+}
+
+function NotificationButton({ saleId }: Props) {
     return (
-        <div className="red-btn-container">
+        <div className="red-btn-container" onClick={() => handleClick(saleId)}>
             <div className="red-btn"><img src={icon} alt="Notificar" /></div>
-        </div>
+        </ div>
     );
 }
 
