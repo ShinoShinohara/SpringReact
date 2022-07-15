282b789f8de0bf72570ee34eeff9c24d9e14a464
shinoshinohara <vshinohara@outlook.com>
2022-07-15 07:46:43 -0400

    toast on screen confirmation for sendnotification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/vector.svg'
 import { BASE_URL } from '../../utils/request';
 import './style.css'
@@ -10,7 +11,7 @@ type Props = {
 
 function handleClick(saleId: number) {
     axios(`${BASE_URL}/sales/${saleId}/notification`).then(response => {
-        console.log("SUCESSO");
+        toast.info("SMS enviado com sucesso");
     })
 }
 
